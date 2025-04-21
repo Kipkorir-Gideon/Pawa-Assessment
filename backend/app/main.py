45cd39e0435007637f81990b3a1d700de5bcb25c
Ngetich Gideon <ngetichgideon21@gmail.com>
2025-04-21 17:47:15 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://query-assistant-ebon.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
